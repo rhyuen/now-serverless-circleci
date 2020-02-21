d5a393476d8adf9e86f049d9112760e25d1d4fd2
robert <dungeonry@hotmail.com>
2020-02-21 04:54:54 -0800

    changing rootpath for url in tests

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,5 +1,6 @@
 const axios = require("axios");
 
+
 describe("test index", () => {
     test("it should return 200", (done) => {
         expect(true).toBe(true)
@@ -9,10 +10,16 @@ describe("test index", () => {
 
 describe("end point", () => {
     it("should respond with text", async (done) => {
-        const response = await axios.get("http://localhost:9911/index");
+        const rootPath = getRootPath();
+        const response = await axios.get(`${rootPath}/index`);
         expect(response.data).toHaveProperty("message");
         expect(response.data.message).toBe("hi, human.");
         done();
     });
 });
 
+function getRootPath() {
+    return process.env.NODE_ENV === "test" ?
+        "http://localhost:9911" :
+        "";
+}
--- __tests__/notfound.spec.ts
+++ __tests__/notfound.spec.ts
@@ -2,9 +2,16 @@ const axios = require("axios");
 
 describe("404", () => {
     it("should respond with 'not found'.", async (done) => {
-        const response = await axios.get("http://localhost:9911/notfound");
+        const rootPath = getRootPath();
+        const response = await axios.get(`${rootPath}/notfound`);
         expect(response.data).toHaveProperty("message");
         expect(response.data.message).toBe("not found, brah.");
         done();
     });
 });
+
+function getRootPath() {
+    return process.env.NODE_ENV === "test" ?
+        "http://localhost:9911" :
+        "";
+}
