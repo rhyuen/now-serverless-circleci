13e7687c6fc6976c4418fac39114e99b5373f21c
robert <dungeonry@hotmail.com>
2020-02-24 02:18:10 -0800

    forgot port 9911 for the dynamic ip.

--- test_helpers/index.ts
+++ test_helpers/index.ts
@@ -4,6 +4,6 @@ exports.getRootPath = () => {
     const serverIp = ip.address();
     console.log(serverIp);
     return process.env.NODE_ENV === "ci" ?
-        `http://${serverIp}` :
+        `http://${serverIp}:9911` :
         "http://127.0.0.1:9911";
 }
