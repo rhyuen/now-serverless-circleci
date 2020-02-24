7203ec788fe1ba4ffa789c0185afb3dbdfd9ffd5
robert <dungeonry@hotmail.com>
2020-02-23 16:15:45 -0800

    minor changes

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -11,7 +11,9 @@ describe("test index", () => {
 
 describe("end point", () => {
     it("should respond with text", async (done) => {
+        console.log(process.env.NODE_ENV);
         const rootPath = getRootPath();
+        console.log(`index ip addres: ${rootPath}`);
         const response = await axios.get(`${rootPath}/index`);
         expect(response.data).toHaveProperty("message");
         expect(response.data.message).toBe("hi, human.");
--- test_helpers/index.ts
+++ test_helpers/index.ts
@@ -3,7 +3,7 @@ const ip = require("ip");
 exports.getRootPath = () => {
     const serverIp = ip.address();
     console.log(serverIp);
-    return process.env.NODE_ENV === "test" ?
-        "http://127.0.0.1:9911" :
-        serverIp;
+    return process.env.NODE_ENV === "ci" ?
+        serverIp :
+        "http://127.0.0.1:9911";
 }
